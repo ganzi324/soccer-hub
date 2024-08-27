5e09f5402f3dd12c7360e97d8816d065929b45af
ganzi324 <ganzi324@gmail.com>
2024-08-27 15:31:05 +0900

    Initialize

---
+++ src/test/java/com/ganzi/soccerhub/SoccerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganzi.soccerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
