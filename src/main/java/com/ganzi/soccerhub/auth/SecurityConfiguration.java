1a17a603f70993c961dc5d88a9f7f019b9cd24d3
ganzi324 <ganzi324@gmail.com>
2024-11-13 22:08:01 +0900

    [feat] User 인증 구현
    #11

---
+++ src/main/java/com/ganzi/soccerhub/auth/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.ganzi.soccerhub.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    private final CustomOAuth2UserService customOauth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headersConfigurer ->
+                        headersConfigurer
+                                .frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::disable
+                                )
+                )
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/")
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOauth2UserService))
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+
+    }
+}
