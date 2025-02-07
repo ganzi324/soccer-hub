d848954cf00786e7e48aa4190b51c99f8f97620a
ganzi324 <ganzi324@gmail.com>
2025-02-07 22:25:32 +0900

    [feat] BCryptPasswordEncoder 빈 추가
    비밀번호 해싱을 위한 인코더 빈 추가

--- src/main/java/com/ganzi/soccerhub/auth/SecurityConfiguration.java
+++ src/main/java/com/ganzi/soccerhub/auth/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,6 +20,11 @@ public class SecurityConfiguration {
     private final CustomOAuth2UserService customOauth2UserService;
 
     @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -29,7 +35,7 @@ public class SecurityConfiguration {
                                 )
                 )
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout((logout) -> logout
