c8e314da507e0c59b641ee3595031af267594d34
ganzi324 <ganzi324@gmail.com>
2025-02-10 16:51:39 +0900

    [feat] 유효성 검사 예외처리 추가

--- src/main/java/com/ganzi/soccerhub/common/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ganzi/soccerhub/common/web/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ganzi.soccerhub.common.web.exception;
 
 import com.ganzi.soccerhub.common.web.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,5 +16,11 @@ public class GlobalExceptionHandler {
         HttpStatus status = exception.getStatus();
 
         return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler({ ConstraintViolationException.class })
+    ResponseEntity<ApiResponse<?>> handleConstraintViolationException(ConstraintViolationException exception) {
+        ApiResponse<?> response = ApiResponse.of(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
