74864ea43e091383c385db1a7c6dc1abd5abb5bb
ganzi324 <ganzi324@gmail.com>
2025-02-11 13:10:34 +0900

    [feat] 요청 파라미터 에러 코드 추가

--- src/main/java/com/ganzi/soccerhub/common/web/exception/ErrorCode.java
+++ src/main/java/com/ganzi/soccerhub/common/web/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     DEFAULT(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
-    DUPLICATE_USER_ID(HttpStatus.BAD_REQUEST, "아이디 중복");
+    DUPLICATE_USER_ID(HttpStatus.BAD_REQUEST, "아이디 중복"),
+    INVALID_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 파라미터가 포함되어 있습니다.");
 
     private final HttpStatus status;
     private final String message;
