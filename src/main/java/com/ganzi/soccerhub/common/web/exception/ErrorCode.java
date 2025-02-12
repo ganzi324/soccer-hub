4882bb69f6a9ed1c4f8406138d3bc85aae3f1c99
ganzi324 <ganzi324@gmail.com>
2025-02-12 11:56:50 +0900

    [feat] 비밀번호 불일치 예외, 에러코드 추가

--- src/main/java/com/ganzi/soccerhub/common/web/exception/ErrorCode.java
+++ src/main/java/com/ganzi/soccerhub/common/web/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     DEFAULT(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
     DUPLICATE_USER_ID(HttpStatus.BAD_REQUEST, "아이디 중복"),
-    INVALID_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 파라미터가 포함되어 있습니다.");
+    INVALID_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 파라미터가 포함되어 있습니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
