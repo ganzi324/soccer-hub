78dbed0aa97cd9fd12207981d7eddf7b2b82e3c3
ganzi324 <ganzi324@gmail.com>
2025-02-14 17:03:19 +0900

    [feat] 인증 과정에서 발생하는 예외 전역 예외로 위임

--- src/main/java/com/ganzi/soccerhub/common/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ganzi/soccerhub/common/web/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.ganzi.soccerhub.common.web.ApiResponseError;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +23,11 @@ public class GlobalExceptionHandler {
     ResponseEntity<ApiResponseError<?>> handleConstraintViolationException(ConstraintViolationException exception) {
         ApiResponseError<?> response = ApiResponseError.of(ErrorCode.INVALID_REQUEST_PARAMETER, exception.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ AuthenticationException.class })
+    ResponseEntity<ApiResponseError<?>> handleAuthenticationException(AuthenticationException exception) {
+        ApiResponseError<?> response = ApiResponseError.of(ErrorCode.DEFAULT, exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 }
