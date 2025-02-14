4270fe0c8c7d36febe94770378549ad32357a809
ganzi324 <ganzi324@gmail.com>
2025-02-14 17:06:39 +0900

    [feat] 보안 설정 추가/변경
    의존성 순환 이슈로 인해 AuthenticationManage SecurityConfiguration 에서 빈 등록
    AuthenticationProvider 빈 등록
    로그인, 토큰 인증 필터 등록(빈으로 처리X)
    예외처리 설정

--- src/main/java/com/ganzi/soccerhub/auth/SecurityConfiguration.java
+++ src/main/java/com/ganzi/soccerhub/auth/SecurityConfiguration.java
@@ -1,24 +1,57 @@
 package com.ganzi.soccerhub.auth;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
     private final CustomOAuth2UserService customOauth2UserService;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final JwtAuthProvider jwtAuthProvider;
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfiguration(
+            AuthenticationConfiguration authenticationConfiguration,
+            CustomOAuth2UserService customOauth2UserService,
+            @Qualifier("delegatedAuthenticationEntryPoint") AuthenticationEntryPoint authenticationEntryPoint,
+            JwtAuthProvider jwtAuthProvider,
+            UserDetailsService userDetailsService
+    ) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.customOauth2UserService = customOauth2UserService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.jwtAuthProvider = jwtAuthProvider;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+        return new CustomAuthenticationProvider(userDetailsService, passwordEncoder);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,17 +64,19 @@ public class SecurityConfiguration {
                                 )
                 )
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/h2-console/**", "/auth/**").permitAll()
+                        .requestMatchers("/h2-console/**", "/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthenticationFilter(jwtAuthProvider, userDetailsService), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(new LoginAuthenticationFilter(authenticationManager(), jwtAuthProvider), UsernamePasswordAuthenticationFilter.class)
                 .logout((logout) -> logout
                         .logoutSuccessUrl("/")
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo.userService(customOauth2UserService))
                 )
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+                .exceptionHandling(configure -> configure.authenticationEntryPoint(authenticationEntryPoint));
         return http.build();
     }
 }
