f0b2968fbb91b34d30b541a49f7210bb8230d002
ganzi324 <ganzi324@gmail.com>
2025-02-16 23:23:54 +0900

    [feat] User 프로필 조회 컨트롤러 추가

--- src/main/java/com/ganzi/soccerhub/user/adaptor/in/web/GetUserController.java
+++ src/main/java/com/ganzi/soccerhub/user/adaptor/in/web/GetUserController.java
@@ -1,5 +1,6 @@
 package com.ganzi.soccerhub.user.adaptor.in.web;
 
+import com.ganzi.soccerhub.auth.SessionUser;
 import com.ganzi.soccerhub.common.WebAdaptor;
 import com.ganzi.soccerhub.common.web.ApiResponse;
 import com.ganzi.soccerhub.user.application.exception.UserNotFoundException;
@@ -8,20 +9,29 @@ import com.ganzi.soccerhub.user.application.response.UserResponse;
 import com.ganzi.soccerhub.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @WebAdaptor
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 class GetUserController {
 
     private final GetUserQuery getUserQuery;
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<ApiResponse<UserResponse>> getUserById(@PathVariable("id") String id) {
         User user = getUserQuery.getUserByEmail(id).orElseThrow(UserNotFoundException::new);
+        return ResponseEntity.ok(ApiResponse.ok(UserResponse.of(user)));
+    }
+
+    @GetMapping("/me")
+    ResponseEntity<ApiResponse<UserResponse>> getMyProfile(@AuthenticationPrincipal SessionUser sessionUser) {
+        User user = getUserQuery.getUserByEmail(sessionUser.email()).orElseThrow(UserNotFoundException::new);
         return ResponseEntity.ok(ApiResponse.ok(UserResponse.of(user)));
     }
 }
