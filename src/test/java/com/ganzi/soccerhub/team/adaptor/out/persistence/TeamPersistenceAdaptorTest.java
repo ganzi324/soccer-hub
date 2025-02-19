4ce1334e374f4115480acf869d677190bc06534d
ganzi324 <ganzi324@gmail.com>
2025-02-19 15:24:47 +0900

    [test] Team 영속성 어댑터 테스트
    Team 생성 테스트

---
+++ src/test/java/com/ganzi/soccerhub/team/adaptor/out/persistence/TeamPersistenceAdaptorTest.java
@@ -0,0 +1,40 @@
+package com.ganzi.soccerhub.team.adaptor.out.persistence;
+
+import com.ganzi.soccerhub.common.TeamTestData;
+import com.ganzi.soccerhub.team.domain.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+public class TeamPersistenceAdaptorTest {
+
+    private TeamPersistenceAdaptor teamPersistenceAdaptor;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @BeforeEach
+    void setUp() {
+        TeamMapper teamMapper = new TeamMapper();
+        teamPersistenceAdaptor = new TeamPersistenceAdaptor(teamRepository, teamMapper);
+    }
+
+    @Test
+    void addTeam_ValidTeam_Succeed() {
+        // given
+        Team newTeam = TeamTestData.defaultTeam();
+
+        Team savedTeam = teamPersistenceAdaptor.save(newTeam);
+
+        // then
+        assertThat(savedTeam.getId()).isEqualTo(newTeam.getId());
+        assertThat(savedTeam.getName()).isEqualTo(newTeam.getName());
+        assertThat(savedTeam.getDescription()).isEqualTo(newTeam.getDescription());
+        assertThat(savedTeam.getCreatedBy()).isEqualTo(newTeam.getCreatedBy());
+    }
+
+}
