f41a9dc199911beca9c008798e45a132a74a302a
ganzi324 <ganzi324@gmail.com>
2025-02-24 16:57:29 +0900

    [fix] ObjectMapper 커스텀 생성 시 기본 옵션이 누락되는 이슈
      Jackson2ObjectMapperBuilder로 생성 시 기본 옵션 적용

--- src/main/java/com/ganzi/soccerhub/common/infra/json/JacksonConfig.java
+++ src/main/java/com/ganzi/soccerhub/common/infra/json/JacksonConfig.java
@@ -2,23 +2,22 @@ package com.ganzi.soccerhub.common.infra.json;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.ganzi.soccerhub.common.infra.hashid.HashIdBeanDeserializerModifier;
 import com.ganzi.soccerhub.common.infra.hashid.HashIdBeanSerializerModifier;
 import com.ganzi.soccerhub.common.infra.hashid.HashIdService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
-    public ObjectMapper objectMapper(HashIdService hashIdService) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder, HashIdService hashIdService) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
 
         SimpleModule module = new SimpleModule();
         module.setSerializerModifier(new HashIdBeanSerializerModifier(hashIdService));
-        module.setDeserializerModifier(new HashIdBeanDeserializerModifier(hashIdService));
-
+//        module.setDeserializerModifier(new HashIdBeanDeserializerModifier(hashIdService));
         objectMapper.registerModule(module);
         return objectMapper;
     }
