a27488f9fd7c0945fbec2987a4a2c9eb1204f92a
ganzi324 <ganzi324@gmail.com>
2025-02-24 16:51:15 +0900

    [feat] RefreshToken 생성 유스케이스 구현

---
+++ src/main/java/com/ganzi/soccerhub/auth/application/service/RefreshTokenService.java
@@ -0,0 +1,29 @@
+package com.ganzi.soccerhub.auth.application.service;
+
+import com.ganzi.soccerhub.auth.application.command.AddRefreshTokenCommand;
+import com.ganzi.soccerhub.auth.application.port.in.AddRefreshTokenUseCase;
+import com.ganzi.soccerhub.auth.application.port.out.AddRefreshTokenPort;
+import com.ganzi.soccerhub.auth.domain.RefreshToken;
+import com.ganzi.soccerhub.common.UseCase;
+import com.ganzi.soccerhub.user.application.exception.UserNotFoundException;
+import com.ganzi.soccerhub.user.application.port.in.GetUserQuery;
+import com.ganzi.soccerhub.user.domain.User;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class RefreshTokenService implements AddRefreshTokenUseCase {
+
+    private final AddRefreshTokenPort addRefreshTokenPort;
+    private final GetUserQuery getUserQuery;
+
+    @Override
+    public RefreshToken addRefreshToken(AddRefreshTokenCommand command) {
+        User user = getUserQuery.getUserById(command.getUserId()).orElseThrow(UserNotFoundException::new);
+
+        RefreshToken refreshToken = RefreshToken.withoutId(user, command.getToken(), command.getExpiresAt());
+        addRefreshTokenPort.save(refreshToken);
+
+        return refreshToken;
+    }
+}
