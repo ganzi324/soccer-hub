08df2eed08a15e8ab764da2fb88adabd2d69e6cb
ganzi324 <ganzi324@gmail.com>
2025-02-25 14:46:25 +0900

    [feat] RefreshToken Repository 생성

--- src/main/java/com/ganzi/soccerhub/auth/adaptor/out/persistence/RefreshTokenRepository.java
+++ src/main/java/com/ganzi/soccerhub/auth/adaptor/out/persistence/RefreshTokenRepository.java
@@ -2,5 +2,8 @@ package com.ganzi.soccerhub.auth.adaptor.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 interface RefreshTokenRepository extends JpaRepository<RefreshTokenJpaEntity, Long> {
+    Optional<RefreshTokenJpaEntity> findByUserIdAndToken(Long userId, String token);
 }
