aa65dd0eaa41a7a3a12576897350a09bdd081642
ganzi324 <ganzi324@gmail.com>
2025-02-25 14:47:20 +0900

    [feat] RefreshToken 영속성 어댑터 조회 추가

--- src/main/java/com/ganzi/soccerhub/auth/adaptor/out/persistence/RefreshTokenPersistenceAdaptor.java
+++ src/main/java/com/ganzi/soccerhub/auth/adaptor/out/persistence/RefreshTokenPersistenceAdaptor.java
@@ -1,13 +1,17 @@
 package com.ganzi.soccerhub.auth.adaptor.out.persistence;
 
 import com.ganzi.soccerhub.auth.application.port.out.AddRefreshTokenPort;
+import com.ganzi.soccerhub.auth.application.port.out.LoadRefreshTokenPort;
 import com.ganzi.soccerhub.auth.domain.RefreshToken;
 import com.ganzi.soccerhub.common.PersistenceAdapter;
+import com.ganzi.soccerhub.user.domain.User;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
-public class RefreshTokenPersistenceAdaptor implements AddRefreshTokenPort {
+public class RefreshTokenPersistenceAdaptor implements AddRefreshTokenPort, LoadRefreshTokenPort {
 
     private final RefreshTokenRepository refreshTokenRepository;
     private final RefreshTokenMapper refreshTokenMapper;
@@ -18,5 +22,11 @@ public class RefreshTokenPersistenceAdaptor implements AddRefreshTokenPort {
         refreshTokenRepository.save(refreshTokenJpaEntity);
 
         return refreshTokenMapper.mapToDomainEntity(refreshTokenJpaEntity);
+    }
+
+    @Override
+    public Optional<RefreshToken> loadByUserIdAndToken(User.UserId userId, String token) {
+        Optional<RefreshTokenJpaEntity> jpaEntity = refreshTokenRepository.findByUserIdAndToken(userId.value(), token);
+        return jpaEntity.map(refreshTokenMapper::mapToDomainEntity);
     }
 }
