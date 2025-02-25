8aaf8f8bac9662de1f306881e740cefe5598f163
ganzi324 <ganzi324@gmail.com>
2025-02-25 14:59:49 +0900

    [fix] 회원가입 요청 객체 필요없는 필드 제거

--- src/main/java/com/ganzi/soccerhub/auth/adaptor/in/web/SignInRequest.java
+++ src/main/java/com/ganzi/soccerhub/auth/adaptor/in/web/SignInRequest.java
@@ -1,5 +1,3 @@
 package com.ganzi.soccerhub.auth.adaptor.in.web;
 
-import com.ganzi.soccerhub.common.infra.hashid.HashId;
-
-public record SignInRequest(String name, String email, String password, @HashId Long id) {}
+public record SignInRequest(String name, String email, String password) {}
