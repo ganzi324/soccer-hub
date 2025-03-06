676cbcfc0373c25f55e08246e06acab69b22cc3a
ganzi324 <ganzi324@gmail.com>
2025-03-06 16:55:07 +0900

    [refactor] JpaAuditing 설정 분리

--- src/main/java/com/ganzi/soccerhub/SoccerHubApplication.java
+++ src/main/java/com/ganzi/soccerhub/SoccerHubApplication.java
@@ -3,11 +3,9 @@ package com.ganzi.soccerhub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
 public class SoccerHubApplication {
 
 	public static void main(String[] args) {
