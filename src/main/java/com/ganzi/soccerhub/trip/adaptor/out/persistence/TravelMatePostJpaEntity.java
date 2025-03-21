30e20d0d1dd4846d3d37d58aaa26e73c687fee26
ganzi324 <ganzi324@gmail.com>
2025-03-21 13:33:26 +0900

    [refactor] TravelMatePost JPA 엔티티 Setter 구성 및 places 접근 메소드 구현
    Getter는 불변객체로 반환하며 객체에 직접 접근 방지

--- src/main/java/com/ganzi/soccerhub/trip/adaptor/out/persistence/TravelMatePostJpaEntity.java
+++ src/main/java/com/ganzi/soccerhub/trip/adaptor/out/persistence/TravelMatePostJpaEntity.java
@@ -6,24 +6,24 @@ import com.ganzi.soccerhub.trip.domain.AgeRange;
 import com.ganzi.soccerhub.user.adaptor.out.persistence.UserJpaEntity;
 import com.ganzi.soccerhub.user.domain.Gender;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
 @Table(name = "travel_mate_post")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TravelMatePostJpaEntity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     @Column(nullable = false, length = 50)
@@ -39,6 +39,7 @@ public class TravelMatePostJpaEntity extends BaseTimeEntity {
             joinColumns = @JoinColumn(name = "travel_mate_post_id"),
             inverseJoinColumns = @JoinColumn(name = "place_id")
     )
+    @Getter(AccessLevel.NONE)
     private List<PlaceJpaEntity> places = new ArrayList<>();
 
     private int capacity;
@@ -56,4 +57,16 @@ public class TravelMatePostJpaEntity extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private UserJpaEntity user;
+
+    public List<PlaceJpaEntity> getPlaces() {
+        return Collections.unmodifiableList(places);
+    }
+
+    public void removePlaces() {
+        places.clear();
+    }
+
+    public void addPlace(PlaceJpaEntity place) {
+        places.add(place);
+    }
 }
