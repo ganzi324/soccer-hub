5bb89fd0a1515cb94ae2c47892c88fc3029d0520
ganzi324 <ganzi324@gmail.com>
2025-03-26 17:47:44 +0900

    [feat] TravelMatePost 검색 서비스 구현

--- src/main/java/com/ganzi/soccerhub/trip/application/service/GetTravelMatePostService.java
+++ src/main/java/com/ganzi/soccerhub/trip/application/service/GetTravelMatePostService.java
@@ -1,10 +1,13 @@
 package com.ganzi.soccerhub.trip.application.service;
 
 import com.ganzi.soccerhub.common.UseCase;
+import com.ganzi.soccerhub.trip.application.command.TravelMatePostSearchCriteria;
 import com.ganzi.soccerhub.trip.application.port.in.GetTravelMatePostQuery;
 import com.ganzi.soccerhub.trip.application.port.out.LoadTravelMatePostPort;
 import com.ganzi.soccerhub.trip.domain.TravelMatePost;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -17,5 +20,10 @@ public class GetTravelMatePostService implements GetTravelMatePostQuery {
     @Override
     public Optional<TravelMatePost> getById(TravelMatePost.PostId id) {
         return loadTravelMatePostPort.loadById(id);
+    }
+
+    @Override
+    public Page<TravelMatePost> search(TravelMatePostSearchCriteria criteria, Pageable pageable) {
+        return loadTravelMatePostPort.search(criteria, pageable);
     }
 }
