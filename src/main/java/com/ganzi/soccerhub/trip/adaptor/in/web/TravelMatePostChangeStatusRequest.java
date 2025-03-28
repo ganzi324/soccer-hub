08cf243e3f4176f5b2521f8098eefd8b203268ae
ganzi324 <ganzi324@gmail.com>
2025-03-28 16:46:11 +0900

    Feat: TravelMatePost 상태 변경 컨트롤러 구현

---
+++ src/main/java/com/ganzi/soccerhub/trip/adaptor/in/web/TravelMatePostChangeStatusRequest.java
@@ -0,0 +1,5 @@
+package com.ganzi.soccerhub.trip.adaptor.in.web;
+
+import com.ganzi.soccerhub.trip.domain.TravelMatePostStatus;
+
+public record TravelMatePostChangeStatusRequest(TravelMatePostStatus status) {}
