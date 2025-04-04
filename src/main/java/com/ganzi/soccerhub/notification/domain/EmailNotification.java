5f87d030576bf8ef9a91bc72f822b849fedabd46
ganzi324 <ganzi324@gmail.com>
2025-04-04 17:58:43 +0900

    Feat: EmailNotification 구현

---
+++ src/main/java/com/ganzi/soccerhub/notification/domain/EmailNotification.java
@@ -0,0 +1,20 @@
+package com.ganzi.soccerhub.notification.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class EmailNotification implements Notification {
+
+    private final EmailReceiver receiver;
+    private final EmailContent content;
+
+    @Override
+    public Content getContent() {
+        return content;
+    }
+
+    @Override
+    public Target getTarget() {
+        return receiver;
+    }
+}
