cc0e836c0a6bedd39766e33288dc4b84d60ca35c
ganzi324 <ganzi324@gmail.com>
2025-04-15 18:00:34 +0900

    Feat: TravelMateJoinRequest 영속성 조회 포트 추가 및 구현
    Post ID, Requester ID 조회

--- src/main/java/com/ganzi/soccerhub/trip/application/port/out/LoadTravelMateJoinRequestPort.java
+++ src/main/java/com/ganzi/soccerhub/trip/application/port/out/LoadTravelMateJoinRequestPort.java
@@ -3,6 +3,7 @@ package com.ganzi.soccerhub.trip.application.port.out;
 import com.ganzi.soccerhub.trip.domain.RequestStatus;
 import com.ganzi.soccerhub.trip.domain.TravelMateJoinRequest;
 import com.ganzi.soccerhub.trip.domain.TravelMatePost;
+import com.ganzi.soccerhub.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -12,4 +13,6 @@ public interface LoadTravelMateJoinRequestPort {
     Optional<TravelMateJoinRequest> loadById(TravelMateJoinRequest.Id id);
 
     Page<TravelMateJoinRequest> loadByPost(TravelMatePost.PostId postId, RequestStatus status, Pageable pageable);
+
+    Optional<TravelMateJoinRequest> loadByPostIdAndRequesterId(TravelMatePost.PostId postId, User.UserId requestId);
 }
