a9b73edeeb4ea339c856ca2520b3e2b55b27c0ca
ganzi324 <ganzi324@gmail.com>
2025-05-19 18:27:07 +0900

    Chore: RestDoc 설정 (#41)

--- src/test/java/com/ganzi/travelmate/common/config/AbstractWebMvcTest.java
+++ src/test/java/com/ganzi/travelmate/common/config/AbstractWebMvcTest.java
@@ -3,14 +3,28 @@ package com.ganzi.travelmate.common.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.ganzi.travelmate.common.infra.hashid.HashIdService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith(RestDocumentationExtension.class)
 public class AbstractWebMvcTest {
 
-    @Autowired
     protected MockMvc mvc;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
 
     protected final ObjectMapper objectMapper = new ObjectMapper()
             .findAndRegisterModules()
--- src/test/java/com/ganzi/travelmate/place/controller/AddPlaceControllerTest.java
+++ src/test/java/com/ganzi/travelmate/place/controller/AddPlaceControllerTest.java
@@ -3,6 +3,7 @@ package com.ganzi.travelmate.place.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ganzi.travelmate.common.config.AbstractWebMvcTest;
 import com.ganzi.travelmate.common.data.PlaceTestData;
+import com.ganzi.travelmate.common.docs.RestDocsCommonFields;
 import com.ganzi.travelmate.place.adaptor.in.web.AddPlaceController;
 import com.ganzi.travelmate.place.application.command.AddPlaceCommand;
 import com.ganzi.travelmate.place.application.port.in.AddPlaceUseCase;
@@ -12,11 +13,14 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,12 +52,40 @@ public class AddPlaceControllerTest extends AbstractWebMvcTest {
         given(addPlaceUseCase.addPlace(any(AddPlaceCommand.class)))
                 .willReturn(place);
 
-        mvc.perform(post("/v1/places")
-                        .with(csrf())
+        mvc.perform(
+                post("/v1/places")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(addPlaceCommand)))
-                        .andExpect(status().isOk())
-                        .andExpect(jsonPath("$.data").exists());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andDo(document("places/add-place",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("장소 이름 (1~20자)"),
+                                fieldWithPath("description").type(JsonFieldType.STRING).optional().description("장소 주소"),
+                                fieldWithPath("state").type(JsonFieldType.STRING).description("주/도 (1~20자"),
+                                fieldWithPath("city").type(JsonFieldType.STRING).description("시/군/구(1~50자)"),
+                                fieldWithPath("street").type(JsonFieldType.STRING).description("도로명 (1~100자)"),
+                                fieldWithPath("detailAddress").type(JsonFieldType.STRING).optional().description("상세 주소"),
+                                fieldWithPath("postalCode").type(JsonFieldType.STRING).description("우편번호 (1~10자)"),
+                                fieldWithPath("latitude").type(JsonFieldType.NUMBER).optional().description("위도"),
+                                fieldWithPath("longitude").type(JsonFieldType.NUMBER).optional().description("경도")
+                        ),
+                        responseFields(
+                                RestDocsCommonFields.commonResponseFields()
+                        ).andWithPrefix("data.",
+                                fieldWithPath("id").description("장소 ID"),
+                                fieldWithPath("name").description("장소 이름"),
+                                fieldWithPath("description").description("장소 설명"),
+                                fieldWithPath("address.state").description("주/도"),
+                                fieldWithPath("address.city").description("시/군/구"),
+                                fieldWithPath("address.street").description("도로명"),
+                                fieldWithPath("address.detailAddress").description("상세 주소"),
+                                fieldWithPath("address.postalCode").description("우편번호"),
+                                fieldWithPath("address.latitude").description("위도"),
+                                fieldWithPath("address.longitude").description("경도"))
+                ));
     }
 
 }
